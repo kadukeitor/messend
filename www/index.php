0134db047668288aeaca060b86dafe466212c5fe
Jorge Rafael Garcia Ramos <kadukeitor@gmail.com>
2015-04-30 11:57:12 -0400

    Starting the Web Inteface

--- www/index.php
+++ www/index.php
@@ -2,18 +2,128 @@
 
 	$f3 = require( 'lib/base.php' );
 
+	$f3->set( 'DEBUG', 3 );
+	$f3->set( 'UI', "views/" );
+	$f3->set( 'TEMP', "data/temp//" );
 	$f3->set( 'CACHE', "folder=data/cache/" );
 	$f3->set( 'LOGS', "data/log/" );
 	$f3->set( 'DB', new DB\Jig( 'data/db/', DB\Jig::FORMAT_JSON ) );
 
 	$f3->route( 'GET /',
 		/**
-		 * Main Route
+		 * Main
 		 *
 		 * @param Base $f3
 		 */
 		function ( \Base $f3 ) {
 			echo "<code>recho</code>";
+		}
+	);
+
+	$f3->route( 'GET /login',
+		/**
+		 * Login
+		 *
+		 * @param Base $f3
+		 */
+		function ( \Base $f3 ) {
+			echo \Template::instance()->render( 'page-login.html' );
+		}
+	);
+
+	$f3->route( 'POST /check_login',
+		/**
+		 * Login
+		 *
+		 * @param Base $f3
+		 */
+		function ( \Base $f3 ) {
+
+			$post = $f3->get( 'POST' );
+			$f3->scrub( $post );
+
+			$key = strtolower( $post['key'] );
+
+			// Session's Mapper
+			$sessions = new DB\Jig\Mapper( $f3->get( 'DB' ), 'sessions' );
+
+			// Checking Key
+			if ( $sessions->load( array( '@_id=?', $key ) ) ) {
+				$f3->set( 'SESSION.key', $key );
+				$f3->reroute( '/dashboard' );
+			} else {
+				$f3->reroute( '/login?' . http_build_query( array( 'error' => base64_encode( "Bad Key" ) ) ) );
+			}
+
+		}
+	);
+
+	$f3->route( 'GET /logout',
+		/**
+		 * Logout
+		 *
+		 * @param Base $f3
+		 */
+		function ( \Base $f3 ) {
+			$f3->clear( 'SESSION' );
+			$f3->reroute( '/login' );
+		}
+	);
+
+	$f3->route( 'GET /dashboard',
+		/**
+		 * Dashboard
+		 *
+		 * @param Base $f3
+		 */
+		function ( \Base $f3 ) {
+
+			if ( ! $f3->exists( 'SESSION.key' ) ) {
+				$f3->reroute( '/login?' . http_build_query( array( 'error' => base64_encode( "Bad Permissions" ) ) ) );
+			}
+
+			$key = $f3->exists( 'SESSION.key' );
+
+			// Session's Mapper
+			$sessions = new DB\Jig\Mapper( $f3->get( 'DB' ), 'sessions' );
+
+			// Sessions
+			$sessions->load( array( '@_id=?', $key ) );
+
+			// Server
+			$server = $sessions->server;
+
+			// Message's Mapper
+			$messages = new DB\Jig\Mapper( $f3->get( 'DB' ), 'messages_' . $server );
+
+			// Messages of the User
+			$messages_user = [ ];
+			foreach (
+				$messages->find( array(
+					'@session=?',
+					$key
+				), array( 'order' => 'datetime SORT_DESC' ) ) as $message
+			) {
+				unset( $message->session );
+				$messages_user[] = $message->cast();
+			}
+
+			// Messages on the Server
+			$messages_server = [ ];
+			foreach (
+				$messages->find( array(
+					'@server=?',
+					$server
+				), array( 'order' => 'datetime SORT_DESC' ) ) as $message
+			) {
+				unset( $message->session );
+				$messages_server[] = $message->cast();
+			}
+
+			$f3->set( 'messages_user', $messages_user );
+			$f3->set( 'messages_server', $messages_server );
+
+			echo \Template::instance()->render( 'page-dashboard.html' );
 		}
 	);
 
